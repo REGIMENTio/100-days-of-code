2918caeca3ca3a68c673fa0fe86ab20c31979b8e
REGIMENTio <tanmayrookie1234@gmail.com>
2025-10-27 22:30:41 +0530

    added q41.c

--- q41.c
+++ q41.c
@@ -2,10 +2,29 @@
 
 #include<stdio.h>
 int main(){
-    int a,b,c,d,e;
+    int a,b,c,d,e,num;
     printf("Enter number to be switched");
     scanf("%d",&a);
-    if (a>10){
-    b=a/10;
-            }
+    
+    b=a%10;
+    num=a;
+
+    d=1;
+    if (a<10){
+        printf("swapped num = %d\n",a);
+    }
+    else {
+    while(a>9){
+       c = a/10;
+       a=a/10;
+       d=d*10;
+    }
+
+ 
+    e=d*b+c*1+num-d*c-b;
+    printf("this is first digit %d\n this is last digit %d\nthis is the swapped number %d\n",c,b,e); 
+    
+
+
+}
 }
